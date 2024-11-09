9085fb7b566fc86709a5da8ba22edf72a8ff7adf
Mirkoanonimo <mirko.lepre@hotmail.com>
2024-11-10 00:44:45 +0100

    storage account+

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.9.0"
     }
   }
+
+   backend "azurerm" {
+    resource_group_name  = "rg-rubrica-dev"
+    storage_account_name = "storubricabackend"
+    container_name      = "tfstate"
+    key                = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
